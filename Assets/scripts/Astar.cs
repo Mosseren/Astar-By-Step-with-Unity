e3f73510cf95b5a0f28bcc678e94500adfd5f6fc
Mosseren <2392263978@qq.com>
2025-09-15 15:52:37 +0800

    init

---
+++ Assets/scripts/Astar.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GridInfo
+{
+    public int g = 0;
+    public int h = 0;
+    public bool visited = false;
+    public bool inPath = false;
+    public int f
+    {
+        get {
+            return g + h;
+        }
+    }
+    public GridInfo(int g, int h, bool visited = false)
+    {
+        this.g = g;
+        this.h = h;
+        this.visited = visited;
+    }
+}
+
+public class Astar
+{
+    Action displayChessBoardDel;
+
+    public Vector2Int source = new Vector2Int(2, 3);
+
+    public Vector2Int target = new Vector2Int(6, 8);
+
+    bool eightDirEnabled = true;
+
+    int[] deltaXs = {-1,  0,  1,  0, -1,  1,  1, -1};
+    int[] deltaYs = { 0, -1,  0,  1, -1, -1,  1,  1};
+    int[] costs   = {10, 10, 10, 10, 14, 14, 14, 14};
+
+    int row = 9;
+    int col = 11;
+    int straightCost = 10;
+    int obliqueCost = 14;
+
+    public bool begun = false;
+    public bool goNextStep = true;
+    public bool finished = false;
+    public bool marked = false;
+
+    int frontierNum = 0;
+
+    public HashSet<Vector2Int> bariers = new HashSet<Vector2Int>();
+
+    public Dictionary<Vector2Int, GridInfo> Grids = new Dictionary<Vector2Int, GridInfo>();
+
+    public Dictionary<Vector2Int, Vector2Int> curToPre = new Dictionary<Vector2Int, Vector2Int>();
+
+    public Astar(Vector2Int start, Vector2Int target, int row, int col, bool eightDirEnabled = true)
+    {
+        this.source = start;
+        this.target = target;
+        this.row = row;
+        this.col = col;
+    }
+
+    public bool IsBarier(Vector2Int loc)
+    {
+        return bariers.Contains(loc);
+    }
+
+    public void ShiftBarier(Vector2Int loc)
+    {
+        if (bariers.Contains(loc)) {
+            bariers.Remove(loc);
+        } else {
+            bariers.Add(loc);
+        }
+    }
+
+
+    public void Clear()
+    {
+        if (begun) {
+            Grids.Clear();
+            curToPre.Clear();
+            begun = false;
+            finished = false;
+            marked = false;
+        }
+    }
+
+    private int  GetH(Vector2Int loc)
+    {
+        int cost;
+        int xDis = Mathf.Abs(loc.x - target.x);
+        int yDis = Mathf.Abs(loc.y - target.y);
+        if (eightDirEnabled) {
+            int obliqueDis = Mathf.Min(xDis, yDis);
+            int straightDis = Mathf.Max(xDis, yDis) - obliqueDis;
+            cost =  obliqueCost * obliqueDis + straightCost * straightDis;
+        } else {
+            cost = straightCost * (xDis + yDis);
+        }
+        
+        return cost;
+    }
+
+    private void GetNeighbours(Vector2Int loc, int currG, ref List<Vector3Int> neibs)
+    {
+        int dirNum = eightDirEnabled ? 8 : 4;
+        for (int i = 0; i < dirNum; ++i) {
+            Vector2Int nextLoc = new Vector2Int(loc.x + deltaXs[i], loc.y + deltaYs[i]);
+            if (bariers.Contains(nextLoc) || nextLoc.x < 0 || nextLoc.x >= row || 
+                nextLoc.y < 0 || nextLoc.y >= col) {
+                    continue;
+            }
+            neibs.Add(new Vector3Int(nextLoc.x, nextLoc.y, currG + costs[i]));
+        }
+    }
+
+    private Vector2Int FindClosestGrid()
+    {
+        Debug.Log("找最近");
+        Vector2Int ans = new Vector2Int(-1, -1);
+        int f = int.MaxValue;
+        foreach (var pair in Grids) {
+            GridInfo info = pair.Value;
+            Vector2Int loc = pair.Key;
+            if (info.visited == false && info.f < f) {
+                f = info.f;
+                ans = loc;
+            }
+        }
+        return ans;
+    }
+
+    public void getDel(Action del)
+    {
+        displayChessBoardDel = del;
+    }
+
+    public void FindPath()
+    {
+        if (!begun) {
+            begun = true;
+            Grids[source] = new GridInfo(0, GetH(source));
+            curToPre[source] = new Vector2Int(-1, -1);
+            ++frontierNum;
+        }
+        if (finished) {
+            if (!marked) {
+                MarkPath();
+                marked = true;
+            }
+            return;
+        }
+        List<Vector3Int> neibs = new List<Vector3Int>();
+
+        Debug.Log(frontierNum);
+        while (frontierNum > 0) {
+            Vector2Int currLoc = FindClosestGrid();
+            Grids[currLoc].visited = true;
+            --frontierNum;
+            if (currLoc == target) {
+                break;
+            }
+            
+            GetNeighbours(currLoc, Grids[currLoc].g, ref neibs);
+            foreach (Vector3Int neib in neibs) {
+                Vector2Int neibLoc = new Vector2Int(neib.x, neib.y);
+                int neibG = neib.z;
+                if (!Grids.ContainsKey(neibLoc)) {
+                    Grids[neibLoc] = new GridInfo(neibG, GetH(neibLoc));
+                    curToPre[neibLoc] = currLoc;
+                    ++frontierNum;
+                } else if (neibG < Grids[neibLoc].g) {
+                    Grids[neibLoc].g = neibG;
+                    curToPre[neibLoc] = currLoc;
+                }
+            }
+            neibs.Clear();
+
+            displayChessBoardDel();
+            return;
+        }
+
+        finished = true;
+        displayChessBoardDel();
+    }
+
+    public void MarkPath()
+    {
+        Vector2Int loc = target;
+        Vector2Int end = new Vector2Int(-1, -1);
+        if (!curToPre.ContainsKey(loc)) {
+            return;
+        }
+        while (loc != end) {
+            Debug.Log("mark");
+            Grids[loc].inPath = true;
+            loc = curToPre[loc];
+        }
+        Grids[source].inPath = true;
+        displayChessBoardDel();
+    }
+}
---
+++ Assets/scripts/GameStart.cs
@@ -0,0 +1,213 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class GameStart : MonoBehaviour
+{
+    Transform canvas;
+    Transform chessBoard;
+    Transform controlPanel;
+    Transform pawn;
+    Transform bishop;
+
+
+    GameObject initGrid;
+
+    Button changeSource;
+    Button changeTarget;
+    Button changeBarier;
+    Button findPath;
+    Button clear;
+
+    [SerializeField]
+    Vector2Int source = new Vector2Int(2, 3);
+    [SerializeField]
+    Vector2Int target = new Vector2Int(6, 8);
+
+    int row = 9;
+    int col = 11;
+
+    Astar astar;
+
+    Color initColor = Color.white;
+    Color visitedColor = Color.blue;
+    Color frontierColor = Color.green;
+    Color pathColor = Color.yellow;
+    Color barierColor = Color.black;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas = FindFirstObjectByType<Canvas>().transform;
+        chessBoard = canvas.Find("ChessBoard").transform;
+        controlPanel = canvas.Find("ControlPanel").transform;
+        pawn = canvas.Find("Pawn").transform;
+        bishop = canvas.Find("Bishop").transform;
+
+        changeSource = controlPanel.Find("ChangeSource").GetComponent<Button>();
+        changeTarget = controlPanel.Find("ChangeTarget").GetComponent<Button>();
+        changeBarier = controlPanel.Find("ChangeBarier").GetComponent<Button>();
+        findPath = controlPanel.Find("FindPath").GetComponent<Button>();
+        clear = controlPanel.Find("Clear").GetComponent<Button>();
+
+        changeSource.onClick.AddListener(ChangeSource);
+        changeTarget.onClick.AddListener(ChangeTarget);
+        changeBarier.onClick.AddListener(ChangeBarier);
+        findPath.onClick.AddListener(FindPath);
+        clear.onClick.AddListener(Clear);
+
+        initGrid = chessBoard.Find("Grid").gameObject;
+        astar = new Astar(source, target, row, col);
+
+        CreateGrids();
+        astar.getDel(DisplayChessBoard);
+    }
+
+    Vector2Int NameToLoc(string name)
+    {
+        Vector2Int loc = new Vector2Int();
+        string[] nums = name.Split('_');
+        loc.x = int.Parse(nums[1]);
+        loc.y = int.Parse(nums[2]);
+        return loc;
+    }
+
+    private void Choosen()
+    {
+        var buttonSelf = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject;
+        Vector2Int loc = NameToLoc(buttonSelf.name);
+        if (changeSource.interactable == false) {
+            if (loc != target && !astar.IsBarier(loc)) {
+                source = loc;
+                if (astar != null) {
+                    astar.source = loc;
+                }
+                pawn.SetParent(buttonSelf.transform, false);
+            }
+            changeSource.interactable = true;
+        }
+
+        if (changeTarget.interactable == false) {
+            if (loc != source && !astar.IsBarier(loc)) {
+                target = loc;
+                if (astar != null) {
+                    astar.target = loc;
+                }
+                bishop.SetParent(buttonSelf.transform, false);
+            }
+            changeTarget.interactable = true;
+        }
+
+        if (changeBarier.interactable == false) {
+            if (loc != source && loc != target && astar != null) {
+                astar.ShiftBarier(loc);
+                Color color = astar.IsBarier(loc) ? barierColor : initColor;
+                DisplayGrid(loc, color);
+            }
+        }
+    }
+
+    private void ChangeSource()
+    {
+        Clear();
+        changeSource.interactable = false;
+        changeTarget.interactable = true;
+        changeBarier.interactable = true;
+    }
+
+    private void ChangeTarget()
+    {
+        Clear();
+        changeTarget.interactable = false;
+        changeSource.interactable = true;
+        changeBarier.interactable = true;
+    }
+
+    private void ChangeBarier()
+    {
+        Clear();
+        changeBarier.interactable = false;
+        changeTarget.interactable = true;
+        changeSource.interactable = true;
+    }
+
+    void CreateGrids()
+    {
+        for (int i = 0; i < row; ++i) {
+            for (int j = 0; j < col; ++j) {
+                GameObject o = Instantiate(initGrid, chessBoard);
+                o.name = string.Format("Grid_{0}_{1}", i, j);
+                o.GetComponent<Button>().onClick.AddListener(Choosen);
+            }
+        }
+        initGrid.SetActive(false);
+        pawn.SetParent(FindGrid(source));
+        pawn.position = Vector3.zero;
+        bishop.SetParent(FindGrid(target));
+        bishop.position = Vector3.zero;
+    }
+
+    Transform FindGrid(Vector2Int loc)
+    {
+        name = string.Format("Grid_{0}_{1}", loc.x, loc.y);
+        return chessBoard.Find(name).transform;
+    }
+
+    void DisplayGrid(Vector2Int loc, Color color)
+    {
+        Button grid = FindGrid(loc).GetComponent<Button>();
+        grid.image.color = color;
+    }
+
+    void DisplayGrid(Vector2Int loc, Color color, int g, int h)
+    {
+        DisplayGrid(loc, color);
+        Transform grid = FindGrid(loc);
+        grid.Find("G").GetComponent<TextMeshProUGUI>().text = g == 0 ? "" : g.ToString();
+        grid.Find("H").GetComponent<TextMeshProUGUI>().text = h == 0 ? "" : h.ToString();
+        grid.Find("F").GetComponent<TextMeshProUGUI>().text = (g + h) == 0 ? "" : (g + h).ToString();
+    }
+
+    void DisplayChessBoard()
+    {
+        foreach (var v in astar.Grids) {
+            Vector2Int loc = v.Key;
+            GridInfo info = v.Value;
+            Color color = info.visited ? visitedColor : frontierColor;
+            if (info.inPath) {
+                color = pathColor;
+            }
+            DisplayGrid(loc, color, info.g, info.h);
+        }
+    }
+
+    void ClearChessBoard()
+    {
+        foreach (var v in astar.Grids) {
+            Vector2Int loc = v.Key;
+            DisplayGrid(loc, initColor, 0, 0);
+        }
+    }
+
+    void FindPath()
+    {
+        changeSource.interactable = true;
+        changeTarget.interactable = true;
+        changeBarier.interactable = true;
+
+        astar.FindPath();
+    }
+
+    void Clear()
+    {
+        changeSource.interactable = true;
+        changeTarget.interactable = true;
+        changeBarier.interactable = true;
+
+        ClearChessBoard();
+        astar.Clear();
+    }
+}
